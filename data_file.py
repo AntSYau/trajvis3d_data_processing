0e3498ac4fb88873baa397792cc78698757e95f0
Qiu Shi <antsyau@outlook.com>
2021-03-04 00:11:23 +0800

    【data_file】完成message生成逻辑

--- data_file.py
+++ data_file.py
@@ -1,4 +1,5 @@
 import protobuf.data_file_pb2 as dfpb
+import protobuf.data_comm_pb2 as dcpb
 import multiprocessing as mp
 import pandas as pd
 import math
@@ -34,9 +35,11 @@ def gcj_to_wgs(lon, lat):
 
 
 def operate_data(gid, uid, data):
-    print("processing gid {:6d} uid {}".format(gid, uid))
-    data = data.loc[(data["lat"] != data["lat"].shift()) |
-                    (data["lng"] != data["lng"].shift())] \
+    if gid % 1000 == 0:
+        print("processing gid {:6d} uid {}".format(gid, uid))
+    data = data.sort_values(by="time") \
+        .loc[(data["lat"] != data["lat"].shift(-1)) |
+            (data["lng"] != data["lng"].shift(-1))] \
         .reset_index(drop=True)
     return uid, data
 
@@ -48,6 +51,7 @@ class DataFile:
         self.data = None
         self.grouped_data = None
         self.groups = None
+        self.instruction_sets=[]
         self.read_file()
 
     def update(self, message):
@@ -60,7 +64,7 @@ class DataFile:
     def read_file(self):
         print("read file {}".format(self.proto.file))
         ts = time.time()
-        header = None if self.proto.has_header else 0
+        header = [0] if self.proto.has_header else []
         self.data = pd.read_csv(
             self.proto.file,
             sep=self.proto.sep,
@@ -68,6 +72,8 @@ class DataFile:
             error_bad_lines=False,
             header=None
         )
+        self.data = self.data.head(100000)
+        print(self.data)
         print("[{:.2f}] rename columns".format(time.time()-ts))
         self.data = self.data.rename(columns={
             self.proto.row_uid: "uid",
@@ -75,6 +81,9 @@ class DataFile:
             self.proto.row_lat: "lat",
             self.proto.row_lng: "lng"
         })[["uid", "time", "lat", "lng"]]
+        self.data["time"]=self.data["time"].astype("int")
+        # self.data["lat"]=self.data["lat"].astype("float")
+        # self.data["lng"]=self.data["lng"].astype("float")
         print("[{:.2f}] drop na".format(time.time()-ts))
         self.data = self.data.dropna().reset_index(drop=True)
         print("[{:.2f}] read file complete".format(time.time()-ts))
@@ -98,8 +107,11 @@ class DataFile:
         for uid, data in tmp:
             u, d = operate_data(gid, uid, data)
             gid+=1
-            self.groups.append(u)
-            self.grouped_data.append(d)
+            if len(d)>0:
+                self.groups.append(u)
+                self.grouped_data.append(d)
+            else:
+                print("group {:6d} is empty!".format(u))
         # for uid, data in tmp:
         #     x = mp.Process(target=operate_data, args=(gid, uid, data,))
         #     gid += 1
@@ -119,6 +131,45 @@ class DataFile:
         #     self.groups.append(uid)
         #     self.grouped_data.append(data)
         print("group data end. time spent: {:.2f}".format(time.time() - ts))
+    
+    def generate_flow(self):
+        instructions = pd.DataFrame()
+
+        for d in self.grouped_data:
+            d["lat_end"]=d["lat"].shift(-1)
+            d["time_end"]=d["time"].shift(-1)
+            d["lng_end"]=d["lng"].shift(-1)
+            d.dropna(inplace=True)
+            d["time_end"]=d["time_end"].astype("int")
+            if d.shape[0]==0: continue
+            d["end"]=0
+            d.iloc[-1, 7]=1
+            instructions=instructions.append(d)
+        
+        instruction_groups=instructions.reset_index(drop=True).groupby("time")
+        self.instruction_sets=[]
+
+        gid=0
+        for ts, tdata in instruction_groups:
+            gid+=1
+            t_inst_set=dcpb.InstructionSet()
+            t_inst_set.timestamp=ts
+            for _, trow in tdata.iterrows():
+                inst=t_inst_set.instructions.add()
+                inst.uid=trow["uid"]
+                inst.start_ts=trow["time"]
+                inst.start_lat=trow["lat"]
+                inst.start_lng=trow["lng"]
+                inst.is_end_instruction=trow["end"]
+                inst.end_ts=trow["time_end"]
+                inst.end_lat=trow["lat_end"]
+                inst.end_lng=trow["lng_end"]
+            self.instruction_sets.append(t_inst_set)
+            if gid % 1000 == 0:
+                print("gid #{}:".format(gid))
+                print(t_inst_set)
+        
+
 
     def gcj_to_wgs(self):
         print("converting coordinate")
